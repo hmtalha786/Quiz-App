b5bab6f2f79195f855873e38602d712c2dd64b72
Muhammad Talha <talhaofficialwork@gmail.com>
2020-12-25 23:39:30 +0500

    First Refined MVP

---
+++ src/App.styles.ts
@@ -0,0 +1,68 @@
+import styled, { createGlobalStyle } from 'styled-components';
+import BGImage from './Images/background.jpg'
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+  body {
+    background-image: url(${BGImage});
+    background-size: cover;
+    background-repeat: no-repeat;
+    margin: 0;
+    padding: 0 20px;
+    display: flex;
+    justify-content: center;
+  }
+  * {
+    font-family: 'Catamaran', sans-serif;
+    box-sizing: border-box;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  > p {
+    color: #fff;
+  }
+  .score {
+    color: #000;
+    font-size: 2rem;
+    margin: 0;
+  }
+  h1 {
+    font-family: Fascinate Inline;
+    background-color: black;
+    font-weight: 400;
+    background-size: 100%;
+    background-clip: text;
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparent;
+    filter: drop-shadow(2px 2px #411f1f);
+    font-size: 5rem;
+    text-align: center;
+    margin: 10% 20px 5% 20px;
+  }
+  @media (max-width: 576px) {
+    h1 {
+      font-size: 3rem;
+    } 
+  }
+  .start, .next {
+    cursor: pointer;
+    background: black;
+    color: #fff;
+    border: 2px solid #000;
+    box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.25);
+    height: 40px;
+    margin: 20px 0;
+    padding: 0 40px;
+  }
+  .start {
+    max-width: 200px;
+  }
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,110 @@
+import React, { useState } from 'react';
+import QuestionCard from './Components/QuestionCard';
+import { fetchQuestions, QuestionState } from './Api'
+import { GlobalStyle, Wrapper } from './App.styles';
+import FormControl from '@material-ui/core/FormControl';
+import NativeSelect from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+
+
+export type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
+
+const Total_Questions = 10;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 120,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  }),
+);
 
 function App() {
+  const classes = useStyles();
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswer, setUserAnswer] = useState<AnswerObject[]>([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+  const [diff, setDiff] = useState("");
+
+  const startTrivia = async () => {
+    setLoading(true);
+    setGameOver(false);
+    const newQuestions = await fetchQuestions(Total_Questions, diff);
+    setQuestions(newQuestions);
+    setScore(0);
+    setUserAnswer([]);
+    setNumber(0);
+    setLoading(false);
+  }
+
+  const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
+
+    if (!gameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer;
+      if (correct) setScore(prev => prev + 1)
+      const answerObject = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer,
+      };
+      setUserAnswer((prev) => [...prev, answerObject])
+    }
+  }
+
+  const nextQuestion = () => {
+    const nextQuestion = number + 1;
+    (nextQuestion === Total_Questions) ? setGameOver(true) : setNumber(nextQuestion) ;
+  }
+
+  const handle = (event: any) => { setDiff(event.target.value); }
   return (
-    <div className="App">
-      Hello World
-    </div>
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <h1>React Quiz</h1>
+        {gameOver || userAnswer.length === Total_Questions ? (
+          <div>
+            <FormControl className={classes.formControl}>
+              <InputLabel htmlFor="age-native-simple">Difficulty</InputLabel>
+              <NativeSelect value={diff} defaultValue="" onChange={handle}>
+                <option style={{ cursor: "pointer" }} value="easy">Easy</option>
+                <option style={{ cursor: "pointer" }} value="medium">Medium</option>
+                <option style={{ cursor: "pointer" }} value="hard">Hard</option>
+              </NativeSelect>
+            </FormControl>
+            <button className="start" onClick={startTrivia}>Start</button>
+          </div>
+        ) : null}
+        {!gameOver ? <p className="score">Score: {score}</p> : null}
+        {loading && <p>Loading Questions...</p>}
+        {!loading && !gameOver && (
+          <QuestionCard
+            questionNr={number + 1}
+            totalQuestions={Total_Questions}
+            question={questions[number].question}
+            answers={questions[number].answers}
+            userAnswer={userAnswer ? userAnswer[number] : undefined}
+            callback={checkAnswer}
+          />)}
+        {!gameOver && !loading && userAnswer.length === number + 1 && number !== Total_Questions - 1 ?
+          <button className="next" onClick={nextQuestion}>Next Question</button> : null}
+      </Wrapper>
+    </>
   );
 }
 
