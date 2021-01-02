ef8c6c3762a8e0357fa009f15fa73b296d738d36
Muhammad Talha <talhaofficialwork@gmail.com>
2021-01-02 17:48:27 +0500

    PWA with Firebase Configuration

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,19 +1,15 @@
-importScripts("https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/7.17.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.17.1/firebase-messaging.js');
 
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("./firebase-messaging-sw.js")
-    .then(function (registration) {
-      console.log("Registration successful, scope is:", registration.scope);
-    })
-    .catch(function (err) {
-      console.log("Service worker registration failed, error:", err);
-    });
-}
+const firebaseConfig = {
+    apiKey: "AIzaSyDqb2OJvwup9AlTTMVWlP7KiSEXMiy2mGk",
+    authDomain: "notificationapp-54b09.firebaseapp.com",
+    databaseURL: "https://notificationapp-54b09.firebaseio.com",
+    projectId: "notificationapp-54b09",
+    storageBucket: "notificationapp-54b09.appspot.com",
+    messagingSenderId: "97986726135",
+    appId: "1:97986726135:web:a2fedc0b07d1975f07abf2"
+  };
 
-firebase.initializeApp({
-  messagingSenderId: "606033826211",
-});
-
-const initMessaging = firebase.messaging();
+  firebase.initializeApp(firebaseConfig);
+  firebase.messaging();
